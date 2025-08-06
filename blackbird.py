8443f90d24d00e318711ae37ee24df1f00bb2180
Gena03 <162874586+Gena03000@users.noreply.github.com>
2025-08-06 12:15:07 +0200

    Update blackbird.py
    Signed-off-by: Gena03 <162874586+Gena03000@users.noreply.github.com>

--- blackbird.py
+++ blackbird.py
@@ -1,5 +1,13 @@
 from pyfiglet import figlet_format
 import os
+import subprocess
+
+def compile_file(file_path):
+    try:
+        subprocess.run(['python', '-m', 'py_compile', file_path], check=True)
+        print(f"✅ Compilation OK : {file_path}")
+    except subprocess.CalledProcessError:
+        print(f"❌ Erreur de compilation : {file_path}")
 
 def scan_repository(repo_path):
     print(figlet_format("Blackbird Scan", font="slant"))
@@ -9,7 +17,8 @@ def scan_repository(repo_path):
             if file.endswith(".py"):
                 full_path = os.path.join(root, file)
                 print(f"→ {full_path}")
-python -m py_compile Blackbird/blackbird.py
+                compile_file(full_path)
+
 
 
 
