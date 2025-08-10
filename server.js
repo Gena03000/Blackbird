07c1b4f3a4492740d0a4820d18b413f75fb0f94e
Gena03 <162874586+Gena03000@users.noreply.github.com>
2025-08-10 17:16:10 +0200

    Update server.js
    Signed-off-by: Gena03 <162874586+Gena03000@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const app = express();
 
-});
+// Port d'écoute (Render utilise process.env.PORT)
+const port = process.env.PORT || 3000;
 
 // Stockage en mémoire des passages
 let passages = [];
 
-
+// Exemple de route
+app.get('/', (req, res) => {
+  res.send('🧶 Serveur textile actif !');
 });
 
 // Démarrage du serveur
